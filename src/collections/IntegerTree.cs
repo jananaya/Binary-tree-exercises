7480b6c7a3acf8b660464d130008355283435d7f
jananaya <jancarlosanayajimenez@gmail.com>
2022-05-08 12:50:38 -0500

    First commit

---
+++ src/collections/IntegerTree.cs
@@ -0,0 +1,58 @@
+﻿using Binary_tree_exercises.types;
+
+namespace Binary_tree_exercises
+{
+    internal class IntegerTree
+    {
+        private BinaryTree<int> _root;
+
+        public IntegerTree()
+        {
+            _root = null;
+        }
+
+        public BinaryTree<int> Root
+        {
+            get
+            {
+                return _root;
+            }
+        }
+
+        public void Insert(int x)
+        {
+            if (_root == null)
+            {
+                _root = new BinaryTree<int>(x);
+                return;
+            }
+
+            Insert(x, _root);
+        }
+
+        private void Insert(int x, BinaryTree<int> r)
+        {
+            if (r == null)
+                return;
+
+            if (x > r.Data)
+            {
+                if (r.Right == null)
+                {
+                    r.LinkRight(new BinaryTree<int>(x));
+                    return;
+                }
+                Insert(x, r.Right);
+            }
+            else
+            {
+                if (r.Left == null)
+                {
+                    r.LinkLeft(new BinaryTree<int>(x));
+                    return;
+                }
+                Insert(x, r.Left);
+            }
+        }
+    }
+}
---
+++ src/types/BinaryTree.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections.Generic;
+
+namespace Binary_tree_exercises.types
+{
+    internal class BinaryTree<E>
+    {
+        private BinaryTree<E> _right;
+        private BinaryTree<E> _left;
+        private E _data;
+
+        public BinaryTree(E data)
+        {
+            _data = data;
+            _right = null;
+            _left = null;
+        }
+
+        public BinaryTree<E> Right
+        {
+            get
+            {
+                return _right;
+            }
+        }
+
+        public BinaryTree<E> Left
+        {
+            get
+            {
+                return _left;
+            }
+        }
+
+        public E Data
+        {
+            get
+            {
+                return _data;
+            }
+        }
+
+        public void LinkRight(BinaryTree<E> r)
+        {
+            _right = r;
+        }
+
+        public void LinkLeft(BinaryTree<E> r)
+        {
+            _left = r;
+        }
+
+        public void Shift(E x)
+        {
+            _data = x;
+        }
+
+        public bool Equal(BinaryTree<E> x)
+        {
+            return Equal(x, this);
+        }
+
+        private bool Equal(BinaryTree<E> x, BinaryTree<E> y)
+        {
+            if (x == null && y == null)
+                return true;
+
+            return x.Data.Equals(y.Data) && Equal(x.Left, y.Left) && Equal(x.Right, y.Right);
+        }
+
+        public bool ShapeEqual(BinaryTree<E> x)
+        {
+            return ShapeEqual(x, this);
+        }
+
+        private bool ShapeEqual(BinaryTree<E> x, BinaryTree<E> y)
+        {
+            if (x == null && y == null)
+                return true;
+
+            return ShapeEqualNode(x, y) && ShapeEqual(x.Left, y.Left) && ShapeEqual(x.Right, y.Right);
+
+        }
+
+        private bool ShapeEqualNode(BinaryTree<E> x, BinaryTree<E> y)
+        {
+            bool shapeEqual;
+
+            if (Exist(x.Left))
+                shapeEqual = Exist(y.Left);
+            else
+                shapeEqual = !Exist(y.Left);
+
+            if (!shapeEqual)
+                return false;
+
+            if (Exist(x.Right))
+                shapeEqual = Exist(y.Right);
+            else
+                shapeEqual = !Exist(y.Right);
+
+            return shapeEqual;
+        }
+
+        private bool Exist(BinaryTree<E> x)
+        {
+            return x != null;
+        }
+    }
+}
